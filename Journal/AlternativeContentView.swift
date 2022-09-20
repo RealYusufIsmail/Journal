4be2add1bae12bdd10e8c662d51e89434ffcdf02
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-09-20 16:17:07 +0100

    added AlternativeContentView

---
+++ Journal/AlternativeContentView.swift
@@ -0,0 +1,13 @@
+//
+// Created by Yusuf Arfan Ismail on 20/09/2022.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct AlternativeContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
